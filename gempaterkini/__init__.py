2a6b3c0dfefb79b8f4d14056afa6c064a037cabb
NelanJoe <nelanjorelat@gmail.com>
2022-10-26 01:05:23 +0700

    add: package

---
+++ gempaterkini/__init__.py
@@ -0,0 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def ekstrasi_data():
+    '''
+    Tanggal: 25 Oktober 2022
+    Waktu: 08:11:23 WIB
+    Magnitudo: 4.4
+    Kedalaman: 10 km
+    Lokasi: 9.02 LS - 117.00 BT
+    Pusat Gempa: Pusat gempa berada di darat 33 km Tenggara Sumbawa Barat
+    Diraasakan: Dirasakan (Skala MMI): III Sumbawa Barat
+    :return:
+    '''
+    try:
+        content = requests.get("https://www.bmkg.go.id/")
+    except Exception:
+        return None
+
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+
+        times = soup.find('span', {'class': 'waktu'})
+        times = times.text.split(', ')
+        waktu = times[1]
+        tanggal = times[0]
+
+        data_content = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        data_content = data_content.findChildren('li')
+
+        i = 0
+        magnitudo = None
+        kedalaman = None
+        koordinat = None
+        lokasi = None
+        dirasakan = None
+
+        for res in data_content:
+            if i == 1:
+                magnitudo = res.text
+            elif i == 2:
+                kedalaman = res.text
+            elif i == 3:
+                koordinat = res.text.split(' - ')
+                ls = koordinat[0]
+                bt = koordinat[1]
+            elif i == 4:
+                lokasi = res.text
+            elif i == 5:
+                dirasakan = res.text
+            i += 1
+
+
+        result = dict()
+        result['tanggal'] = tanggal
+        result['waktu'] = waktu
+        result['magnitudo'] = magnitudo
+        result['kedalaman'] = kedalaman
+        result['koordinat'] = {'ls': ls, 'bt': bt}
+        result['lokasi'] = lokasi
+        result['dirasakan'] = dirasakan
+
+        return result
+
+    else:
+        return None
+
+
+def tampilkan_data(result):
+    if result is None:
+        print("Tidak bisa menemukan data terkini")
+        return
+
+    print('Gempa berdasarkan BMKG')
+    print(f"Tanggal {result['tanggal']}")
+    print(f"Waktu {result['waktu']}")
+    print(f"Magnitudo {result['magnitudo']}")
+    print(f"Kedalaman {result['kedalaman']}")
+    print(f"Koordinat LS:{result['koordinat']['ls']} BT:{result['koordinat']['bt']} ")
+    print(f"Lokasi:  {result['lokasi']}")
+    print(f"Dirasakan {result['dirasakan']}")
--- main.py
+++ main.py
@@ -2,41 +2,9 @@
     * Aplikasi Gempa Terkini
     * MODULARISASI DENGAN FUNCTION
 '''
-
-
-def ekstrasi_data():
-    '''
-    Tanggal: 25 Oktober 2022
-    Waktu: 08:11:23 WIB
-    Magnitudo: 4.4
-    Kedalaman: 10 km
-    Lokasi: 9.02 LS - 117.00 BT
-    Pusat Gempa: Pusat gempa berada di darat 33 km Tenggara Sumbawa Barat
-    Diraasakan: Dirasakan (Skala MMI): III Sumbawa Barat
-    :return:
-    '''
-    result = dict()
-    result['tanggal'] = '25 Oktober 2022'
-    result['waktu'] = '08:11:23 WIB'
-    result['magnitudo'] = 4.4
-    result['lokasi'] = {'ls': 1.48, 'bt': 131.01}
-    result['pusat'] = 'Pusat gempa berada di darat 33 km Tenggara Sumbawa Barat'
-    result['dirasakan'] = 'Dirasakan (Skala MMI): III Sumbawa Barat'
-
-    return result
-
-
-def tampilkan_data(result):
-    print('Gempa berdasarkan BMKG')
-    print(f"Tanggal {result['tanggal']}")
-    print(f"Waktu {result['waktu']}")
-    print(f"Magnitudo {result['magnitudo']}")
-    print(f"Lokasi:  LS:{result['lokasi']['ls']}, BT:{result['lokasi']['bt']}")
-    print(f"Pusat {result['pusat']}")
-    print(f"Dirasakan {result['dirasakan']}")
-
+import gempaterkini
 
 if __name__ == '__main__':
-    print("Tampilkan data")
-    result = ekstrasi_data()
-    tampilkan_data(result)
+    print("Aplikasi Gempa Terkini")
+    result = gempaterkini.ekstrasi_data()
+    gempaterkini.tampilkan_data(result)
